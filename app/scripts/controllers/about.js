596ae3d8e939292c72544aa61d1356148ae40147
Joey Benenati <joey.benenati@gmail.com>
2015-12-17 14:50:27 -0600

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name geckoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the geckoApp
+ */
+angular.module('geckoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
